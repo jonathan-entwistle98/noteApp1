bade44ffa557c82cb7469eb3796c8b8bbcf40cbf
jje1g16 <jje1g16@JFB59-R3229-J06.soton.ac.uk>
2018-01-29 15:41:30 +0000

    Added JavaDoc Comment to start method

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	/* 
+	 * Creates a new Scene, adds this scene to the stage and sets the css and fxml files to be used.
+	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 		primaryStage.setTitle("Notes Application");
 		Scene scene = new Scene(root, 800, 500);
 		scene.getStylesheets().add("resource/test.css");
-		// scene.getStylesheets().add(getClass().getResource(name))
 		primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("icon.png")));
 		primaryStage.setScene(scene);
 		primaryStage.show();
